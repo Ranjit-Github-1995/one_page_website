53e933cc5350363105f37585ec48a4fdf61f859d
Ranjit-Github-1995 <63495068+Ranjit-Github-1995@users.noreply.github.com>
2020-04-12 09:07:17 +0530

    ff

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+
+	
+	$('#video_screen1').videoBG({
+		mp4:'assets/video_screen1.mp4',
+		ogv:'assets/video_screen1.ogv',
+		webm:'assets/video_screen1.webm',
+		poster:'assets/video_screen1.gif',
+		scale:true,
+		zIndex:0
+	});
+	
+	
+	
+		
+})
